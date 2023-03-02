5ae7be48bf61391ccbbbd130d47754bd0ee2b84f
jrhowell123 <97644035+jrhowell123@users.noreply.github.com>
2023-03-01 18:38:58 -0800

    Part 2 finished

--- JohnHowellPersonalPortfolio/customdialog.js
+++ JohnHowellPersonalPortfolio/customdialog.js
@@ -1,3 +1,28 @@
-export function sayHi(user) {
-    return `Hello, ${user}!`;
+export function showDialog() {
+    alertDialog.showModal();
   }
+
+export function showConfirmDialog() {
+    confirmDialog.showModal();
+}
+export function showPromptDialog() {
+    promptDialog.showModal();
+}
+
+export function confirmed() {
+    const result = true
+    document.getElementById('change').innerHTML = `The value returned by the confirm method is: ${result}`;
+}
+
+export function notConfirmed() {
+    const result = false
+    document.getElementById('change').innerHTML = `The value returned by the confirm method is: ${result}`;
+}
+
+export function promptInput(param,param1) {
+    param1.innerHTML = param.value;
+}
+
+export function promptCancel(param) {
+    param.innerHTML = 'No Input!';
+}
